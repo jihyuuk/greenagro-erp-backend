75474e35f4288277fa8c3d1504d62528fd0e36e7
jihyuuk <jihyuk.dev@gmail.com>
2025-07-22 15:12:41 +0900

    chore : init project

---
+++ src/main/java/erp/greenagro/greenagro_erp_backend/GreenagroErpBackendApplication.java
@@ -0,0 +1,13 @@
+package erp.greenagro.greenagro_erp_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenagroErpBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreenagroErpBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/erp/greenagro/greenagro_erp_backend/GreenagroErpBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package erp.greenagro.greenagro_erp_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenagroErpBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
