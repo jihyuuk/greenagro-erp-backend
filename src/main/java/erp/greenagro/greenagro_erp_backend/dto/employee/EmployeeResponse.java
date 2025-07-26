8065424f4fe8f6b683ffe67d8b2033489366caf0
jihyuuk <jihyuk.dev@gmail.com>
2025-07-26 14:16:57 +0900

    #1 feat : 직원 기본 DTO 생성

---
+++ src/main/java/erp/greenagro/greenagro_erp_backend/dto/employee/EmployeeResponse.java
@@ -0,0 +1,29 @@
+package erp.greenagro.greenagro_erp_backend.dto.employee;
+
+import erp.greenagro.greenagro_erp_backend.model.entity.Branch;
+import erp.greenagro.greenagro_erp_backend.model.enums.AccountStatus;
+import erp.greenagro.greenagro_erp_backend.model.enums.Role;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.time.LocalDate;
+
+@Getter
+@AllArgsConstructor
+public class EmployeeResponse {
+
+    private Long id; //직원번호
+    private Long branchId; //지점번호
+    private String branchName; //지점명
+    private String name; //이름
+    private String position; //직위
+    private String phone; //전화번호
+    private String email; //이메일
+    private LocalDate hireDate; //입사일자
+    private LocalDate retireDate; //퇴사일자
+    private Role role; //권한
+    private AccountStatus accountStatus; //계정상태
+
+}
--- src/main/java/erp/greenagro/greenagro_erp_backend/mapper/EmployeeMapper.java
+++ src/main/java/erp/greenagro/greenagro_erp_backend/mapper/EmployeeMapper.java
@@ -2,6 +2,7 @@ package erp.greenagro.greenagro_erp_backend.mapper;
 
 import erp.greenagro.greenagro_erp_backend.dto.employee.CreateEmployeeRequest;
 import erp.greenagro.greenagro_erp_backend.dto.employee.CreateEmployeeResponse;
+import erp.greenagro.greenagro_erp_backend.dto.employee.EmployeeResponse;
 import erp.greenagro.greenagro_erp_backend.model.entity.Branch;
 import erp.greenagro.greenagro_erp_backend.model.entity.Employee;
 import erp.greenagro.greenagro_erp_backend.model.enums.AccountStatus;
@@ -13,12 +14,12 @@ public class EmployeeMapper {
     /**
      * CreateEmployeeRequest ===> Employee 엔티티로 변환합니다.
      */
-    public Employee toEntity(CreateEmployeeRequest request, Branch branch, String hashedPwd, String encryptedRrn){
+    public Employee toEntity(CreateEmployeeRequest request, Branch branch, String hashedPwd, String encryptedRrn) {
         return new Employee(
                 branch,                 // 지점
                 request.getName(),      // 이름
                 hashedPwd,              // 비밀번호
-                encryptedRrn,             // 주민번호
+                encryptedRrn,           // 주민번호
                 request.getPosition(),  // 직위
                 request.getPhone(),     // 핸드폰
                 request.getEmail(),     // 이메일
@@ -33,8 +34,24 @@ public class EmployeeMapper {
     /**
      * Employee 엔티티 ===> CreateEmployeeResponse DTO 로 변환합니다.
      */
-    public CreateEmployeeResponse toResponse(Employee employee, String tempPwd){
+    public CreateEmployeeResponse toResponse(Employee employee, String tempPwd) {
         return new CreateEmployeeResponse(employee.getId(), tempPwd);
+    }
+
+    public EmployeeResponse toResponse(Employee employee) {
+        return new EmployeeResponse(
+                employee.getId(),               //직원번호
+                employee.getBranch().getId(),   // 지점번호
+                employee.getBranch().getName(), // 지점명
+                employee.getName(),             // 이름
+                employee.getPosition(),         // 직위
+                employee.getPhone(),            // 핸드폰
+                employee.getEmail(),            // 이메일
+                employee.getHireDate(),         // 입사일자
+                employee.getRetireDate(),       // 입사일자
+                employee.getRole(),             // 권한
+                employee.getStatus()            // 계정상태 활성화
+        );
     }
 
 }
