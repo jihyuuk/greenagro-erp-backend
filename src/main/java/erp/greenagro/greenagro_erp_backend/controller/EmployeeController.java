58c49d31c3f77b52e8c0d64790cbeaeb5346b83f
jihyuuk <jihyuk.dev@gmail.com>
2025-07-26 13:36:35 +0900

    #1 feat :  직원등록 API 구현 완료 ( POST /employees/new )

--- src/main/java/erp/greenagro/greenagro_erp_backend/controller/EmployeeController.java
+++ src/main/java/erp/greenagro/greenagro_erp_backend/controller/EmployeeController.java
@@ -1,11 +1,27 @@
 package erp.greenagro.greenagro_erp_backend.controller;
 
+import erp.greenagro.greenagro_erp_backend.dto.employee.CreateEmployeeRequest;
+import erp.greenagro.greenagro_erp_backend.dto.employee.CreateEmployeeResponse;
+import erp.greenagro.greenagro_erp_backend.service.EmployeeService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.http.HttpStatus.*;
 
 @RestController
 @RequiredArgsConstructor
 public class EmployeeController {
 
+    private final EmployeeService employeeService;
+
+    @PostMapping("/employees/new")
+    public ResponseEntity<CreateEmployeeResponse> createEmployee(@RequestBody CreateEmployeeRequest request){
+        CreateEmployeeResponse response = employeeService.createEmployee(request);
+        return ResponseEntity.status(CREATED).body(response);
+    }
 
 }
