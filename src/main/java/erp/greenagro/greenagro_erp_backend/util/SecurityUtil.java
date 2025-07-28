b5308cdaf50894b1ef4261e25dc4a5b1c7947177
jihyuuk <jihyuk.dev@gmail.com>
2025-07-28 15:06:19 +0900

    #1 feat : 주민등록번호 복호화 메서드 구현

--- src/main/java/erp/greenagro/greenagro_erp_backend/util/SecurityUtil.java
+++ src/main/java/erp/greenagro/greenagro_erp_backend/util/SecurityUtil.java
@@ -48,4 +48,23 @@ public class SecurityUtil {
             throw new RuntimeException("주민등록번호 암호화 실패", e);
         }
     }
+
+    /**
+     * 주민등록번호 복호화
+     * @param encryptedRrn AES 암호화된 주민등록번호
+     * @return 평문 주민등록번호
+     */
+    public static String decryptRrn(String encryptedRrn){
+        try {
+            String AES_KEY_RRN = System.getenv("AES_KEY_RRN");
+            SecretKeySpec key = new SecretKeySpec(AES_KEY_RRN.getBytes(), "AES");
+            Cipher cipher = Cipher.getInstance("AES");
+            cipher.init(Cipher.DECRYPT_MODE, key);
+            byte[] decoded = Base64.getDecoder().decode(encryptedRrn);
+            byte[] decrypted = cipher.doFinal(decoded);
+            return new String(decrypted);
+        } catch (Exception e) {
+            throw new RuntimeException("주민등록번호 복호화 실패", e);
+        }
+    }
 }
