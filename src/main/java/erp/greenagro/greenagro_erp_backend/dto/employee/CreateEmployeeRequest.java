62b06f95596025cbf3b097590126471d4e2a05fd
jihyuuk <jihyuk.dev@gmail.com>
2025-07-30 12:24:00 +0900

    #1 refactor : dto에 @Enumerated 제거함

--- src/main/java/erp/greenagro/greenagro_erp_backend/dto/employee/CreateEmployeeRequest.java
+++ src/main/java/erp/greenagro/greenagro_erp_backend/dto/employee/CreateEmployeeRequest.java
@@ -2,8 +2,11 @@ package erp.greenagro.greenagro_erp_backend.dto.employee;
 
 import erp.greenagro.greenagro_erp_backend.dto.payinfo.PayInfoDTO;
 import erp.greenagro.greenagro_erp_backend.model.enums.Role;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -22,7 +25,6 @@ public class CreateEmployeeRequest {
     private String address; //주소
     private LocalDate hireDate; //입사일자
 
-    @Enumerated(EnumType.STRING)
     private Role role; //권한
 
     private PayInfoDTO payInfo; //급여정보(은행,계좌,예금주,기본급)
