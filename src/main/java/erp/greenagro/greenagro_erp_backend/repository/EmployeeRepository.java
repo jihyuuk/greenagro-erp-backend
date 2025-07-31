b250ae5c08de89b4ac8689dd1c42dca991146105
jihyuuk <jihyuk.dev@gmail.com>
2025-07-31 23:31:09 +0900

    #1 feat : EmployeeRepository에 직원 이름으로 조회하는 findByName() 메소드 추가함

--- src/main/java/erp/greenagro/greenagro_erp_backend/repository/EmployeeRepository.java
+++ src/main/java/erp/greenagro/greenagro_erp_backend/repository/EmployeeRepository.java
@@ -4,6 +4,11 @@ import erp.greenagro.greenagro_erp_backend.model.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+    Optional<Employee> findByName(String name);
+
 }
