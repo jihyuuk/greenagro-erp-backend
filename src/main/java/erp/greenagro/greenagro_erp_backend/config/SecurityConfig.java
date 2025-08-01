870776d60d2a891d3394e1a3d8c18d48ae75c672
jihyuuk <jihyuk.dev@gmail.com>
2025-08-01 16:42:52 +0900

    #3 feat : SecurityConfig에 JWT 인증 필터 등록함

--- src/main/java/erp/greenagro/greenagro_erp_backend/config/SecurityConfig.java
+++ src/main/java/erp/greenagro/greenagro_erp_backend/config/SecurityConfig.java
@@ -1,25 +1,35 @@
 package erp.greenagro.greenagro_erp_backend.config;
 
+import erp.greenagro.greenagro_erp_backend.filter.JwtAuthenticationFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests(auth -> auth
+        return http
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class) //JWT 필터 추가
+                .csrf(csrf -> csrf.disable()) // CSRF 비활성화
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) //세션 사용 안함
+                .authorizeHttpRequests(auth -> auth //엔드 포인드 설정
+                        //.requestMatchers("/login").permitAll()
+                        //.anyRequest().authenticated()
                         .anyRequest().permitAll()
-                )
-                .csrf(csrf -> csrf.disable());
-        return http.build();
+                ).build();
     }
 
     @Bean
