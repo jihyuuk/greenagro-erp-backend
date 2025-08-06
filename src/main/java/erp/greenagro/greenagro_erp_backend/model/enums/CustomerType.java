650794c65cee6d14161e44bf4e1737a1345e933e
jihyuuk <jihyuk.dev@gmail.com>
2025-08-06 16:23:58 +0900

    #5 refactor : CustomerType 필드 CORPORATE -> CORPORATION으로 수정

--- src/main/java/erp/greenagro/greenagro_erp_backend/model/enums/CustomerType.java
+++ src/main/java/erp/greenagro/greenagro_erp_backend/model/enums/CustomerType.java
@@ -2,7 +2,7 @@ package erp.greenagro.greenagro_erp_backend.model.enums;
 
 public enum CustomerType {
 
-    CORPORATE,          // 법인사업자
+    CORPORATION,        // 법인사업자
     SOLE_PROPRIETOR,    // 개인사업자
     INDIVIDUAL          // 개인
 
